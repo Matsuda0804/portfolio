eb560d06072a52b816663a6b00226f9b121b05ca
Matsuda0804 <56781959+Matsuda0804@users.noreply.github.com>
2020-10-19 12:28:15 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,61 @@
+// PhotoSwipe
+initPhotoSwipeFromDOM(".js-my-gallery");
+
+$(function () {
+
+  //iOS対策
+  //iOSでは疑似要素を含むaタグのリンクは２回タップしないと遷移とページ内スクロールをしないため、
+  //ユーザーエージェント判定でiOSの場合はbodyタグにiosを付与し、対象の疑似要素をdisplay: noneする
+  var ua = navigator.userAgent;
+  if (/iPhone|iPad|iPod/.test(ua)) {
+    $("body").addClass("ios");
+  }
+
+  //Worksのリンクを有効化
+  //スライド（Swiper）内に記載のリンクを有効にするため下記の記述が必要 (;´･ω･)ｳｰﾝ･･･
+  $(".works-url").on("click", "a", function (e) {
+    e.stopPropagation();
+  });
+
+  //ページ内スクロール
+  var $nav = $(".gnav");
+  var navHeight = $nav.outerHeight();
+
+  $('a[href^="#"]').on("click", function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top - navHeight;
+    $("html, body").animate(
+      {
+        scrollTop: position,
+      },
+      500,
+      "swing"
+    );
+    return false;
+  });
+
+});
+
+$(function() {
+    var topBtn = $('#PageTop');
+    //ボタンを非表示にする
+    topBtn.hide();
+    //スクロールしてページトップから100に達したらボタンを表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+　　　　　　　//フェードインで表示
+            topBtn.fadeIn();
+        } else {
+　　　　　　　//フェードアウトで非表示
+            topBtn.fadeOut();
+        }
+    });
+    //スクロールしてトップへ戻る
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
